15b69cfffce48e3b0e44d3d7c544914682cc1804
Ali Reza Eshaqi <eshaqi.alireza@gmail.com>
2024-08-26 16:55:39 +1000

    Section 8 - Sunentities - implement dishes

---
+++ Restaurants.Application/Dishes/Validators/CreateDishCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using Restaurants.Application.Dishes.Commands.CreateDish;
+
+namespace Restaurants.Application.Dishes.Validators;
+
+public class CreateDishCommandValidator : AbstractValidator<CreateDishCommand>
+{
+    public CreateDishCommandValidator()
+    {
+        RuleFor(dish => dish.Price).GreaterThanOrEqualTo(0).WithMessage("Price must be non-negative number");
+        RuleFor(dish => dish.KiloCalories).GreaterThanOrEqualTo(0).WithMessage("Kilo Calories must be non-negative number");
+    }
+}
